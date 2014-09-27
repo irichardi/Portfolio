f6691a8fa7dd3930250ed7bcf1712b4cd2c9ef43
Richard <irichardi@live.ca>
2014-09-26 23:24:33 -0400

    Few tweaks
    Fix a couple of css errors

--- github.php
+++ github.php
@@ -17,7 +17,8 @@ description:the start of my website, home containing an intro. -->
 </head>
 <?php include "header.php";?>
 <h1 class="columns" style="text-align:center">Github</h1>
-<div id="gitlink" class="small-12 medium-1 large-1 columns">
+<p class="small-12 medium-12 large-12 small-centered columns" style="text-align:center;">Click the botton below to enter my Github Repository!</p>
+<div id="gitlink" class="small-1 medium-1 large-1 small-centered columns">
 <a href="https://github.com/irichardi/Portfolio" class="button">Enter Github</a>
 </div>
 <?php include("footer.php") ?>
